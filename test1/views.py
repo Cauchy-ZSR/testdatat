bfff2b5e109c6fcb373c234ac5445a361608a4f3
Cauchy <1989004969@qq.com>
2022-02-25 21:03:42 +0800

    success

--- test1/views.py
+++ test1/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import  viewsets
+from rest_framework.decorators import api_view
 from .models import demo
 from .serializers import demoSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import BasicAuthentication
 # Create your views here.
 
 
@@ -15,6 +17,12 @@ class demoViewSet(viewsets.ModelViewSet):
     queryset = demo.objects.all().order_by('age')
     # 序列化器
     serializer_class = demoSerializer
+
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
 
 
 class loginView(APIView):
